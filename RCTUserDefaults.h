ce490d2169c6642e0372b19c89234c756c1312d8
Guilherme da Silva Mello <guilhermesilvamello@gmail.com>
2018-03-12 15:08:03 -0300

    Allow RN 0.40+

--- RCTUserDefaults.h
+++ RCTUserDefaults.h
@@ -1,4 +1,10 @@
+#if __has_include(<React/RCTBridge.h>)
+#import <React/RCTBridge.h>
+#elif __has_include("RCTBridge.h")
 #import "RCTBridge.h"
+#else
+#import "React/RCTBridge.h"   // Required when used as a Pod in a Swift project
+#endif
 
 @interface RCTUserDefaults : NSObject <RCTBridgeModule>
 
